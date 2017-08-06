e681416986f6d969487399cb4b5fbbbc2d31da00
aigars-jekabsons <aigarsaj@gmail.com>
2017-08-06 23:19:12 +0100

    node backend

---
+++ server/routes/api.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+// declare axios for making http requests
+const axios = require('axios');
+const API = 'https://jsonplaceholder.typicode.com';
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+// Get all posts
+router.get('/posts', (req, res) => {
+  // Get posts from the mock api
+  // This should ideally be replaced with a service that connects to MongoDB
+  axios.get(`${API}/posts`)
+    .then(posts => {
+      res.status(200).json(posts.data);
+    })
+    .catch(error => {
+      res.status(500).send(error)
+    });
+});
+
+module.exports = router;
+
+
+
+
+
+    var Connection = require('tedious').Connection;  
+    var config = {  
+        userName: 'yourusername',  
+        password: 'yourpassword',  
+        server: 'yourserver.database.windows.net',  
+        // If you are on Microsoft Azure, you need this:  
+        options: {encrypt: true, database: 'AdventureWorks'}  
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+    // If no error, then good to proceed.  
+        console.log("Connected");  
+    }); 
